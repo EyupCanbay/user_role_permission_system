a91edfc2200b3b1d0aaeae1f7f83ceac2caa0ffd
EyupCanbay <eyupcanbay40@gmail.com>
2025-03-04 18:36:30 +0300

    Add category update

--- controllers/category_controller.js
+++ controllers/category_controller.js
@@ -1,10 +1,10 @@
 const ResponseHandler = require('../lib/responseHandler.js');
 const CustomError = require('../lib/customError.js');
-const Roles = require('../models/Roles.js')
 const Category = require("../models/Categories.js")
+
 async function getAllCategories(req,res,next){
     try{    
-        let categories =await Roles.find();
+        let categories =await Category.find();
         
         res.status(200).json(ResponseHandler.success('Categories retrieved successfully', categories));
     } catch (error) {
@@ -22,15 +22,37 @@ async function createCategory(req,res,next){
             created_by: body.user?.id
         })
         await category.save()
-        
+
         res.status(201).json(ResponseHandler.success('Category created successfully', category)); 
     } catch (error) {
         res.status(500).json(ResponseHandler.error('An error occurred', error));
     }
 }
 
+async function updateCategory(req,res,next){
+    const category_id = req.params.category_id;
+
+    try{
+
+        const category = await Category.findByIdAndUpdate(
+            req.params.category_id,
+            {
+                name: req.body.name,
+                is_active: req.body.is_active,
+                created_by: req.body.user?.id
+            },
+            { new: true}
+        )
+        
+        res.status(200).json(ResponseHandler.success('Category updated successfully', category)); 
+    } catch (error) {
+        next(error)
+    }
+
+}
 
 module.exports = {
     getAllCategories,
-    createCategory
+    createCategory,
+    updateCategory
 }
--- routes/category_routes.js
+++ routes/category_routes.js
@@ -4,6 +4,7 @@ const categoryController = require('../controllers/category_controller');
 
 router.get('/', categoryController.getAllCategories);
 router.post('/', categoryController.createCategory);
+router.put('/:category_id', categoryController.updateCategory);
 
 
 
